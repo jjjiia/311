270f4e8d1b3415fa90be7996604ccfdb04826974
jz3077 <jz3077@columbia.edu>
2019-02-22 07:43:15 -0500

    first commit

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var boroughChart = dc.rowChart("#borough-chart");
 var dayOfWeekChart = dc.rowChart("#day-of-week-chart");
 var agencyChart = dc.rowChart("#agency-chart");
 var hourChart = dc.barChart("#hour-chart");
-var tempChart = dc.barChart("#temperature-chart")
 var nycMap = dc.geoChoroplethChart("#nyc-chart");
 var zipcodeChart = dc.rowChart("#zipcode-chart");
+//var durationChart = dc.barChart("#duration-chart");
 //var moveChart = dc.lineChart("#monthly-move-chart");
 //var volumeChart = dc.barChart("#monthly-volume-chart");
 //var yearlyBubbleChart = dc.bubbleChart("#yearly-bubble-chart");
@@ -26,7 +26,7 @@ function toTitleCase(str)
 
 
 queue()
-.defer(d3.csv, "nyc_2013_ALL.csv")
+.defer(d3.csv, "311_data.csv")
 //.defer(d3.csv, "nyc_smallSample.csv")
 .defer(d3.json, "nyc-zip-codes.geojson")
 .await(ready);
@@ -39,72 +39,106 @@ var tlc = "TLC"
 
 function ready(error, data, geodata){
 	//format dates
-    var dateFormat = d3.time.format("%m%d%Y");
     var numberFormat = d3.format(".2f");
 	
 	//prepars date and month
-    data.forEach(function (d) {
-        d.dd = dateFormat.parse(d.date);
-        d.month = d3.time.month(d.dd); // pre-calculate month for better performance
-    });
+   // data.forEach(function (d) {
+   //     d.dd = Date.parse(d["Created Date"]);
+   //     var date = new Date(d["Created Date"]);
+   //     d.month = date.getMonth()+1; 
+   //     d.hour = date.getHours()+1;
+   //     d.day = date.getDay()
+   // });
 
-    //### Create Crossfilter Dimensions and Groups
     //See the [crossfilter API](https://github.com/square/crossfilter/wiki/API-Reference) for reference.
     var ndx = crossfilter(data);
     var all = ndx.groupAll();
 
     // dimension by month
     var monthDimension = ndx.dimension(function (d) {
-        return d.month;
+        var date = new Date(d["Created Date"]);
+        return date.getMonth()+1;
     });
   
-    var dateDimension = ndx.dimension(function (d) {
-        return d.dd;
-    });
-
-	var temp = ndx.dimension(function(d){
-		return d.tempMax
-	})
-	var tempGroup = temp.group();
-    // create categorical dimension
+//    var dateDimension = ndx.dimension(function (d) {
+//        var date = new Date(d["Created Date"]);
+//        return d.getDate();
+//    });
+//
+	
     var agency = ndx.dimension(function (d) {
-        return d.agency;
+        return d["Agency"];
     });
     var agencyGroup = agency.group();
 
     var complaint = ndx.dimension(function (d) {
-        return d.description;
+        return d["Descriptor"];
     });
     var complaintGroup = complaint.group();
   
     var hour = ndx.dimension(function (d) {
-        return d.hour;
+        var date = new Date(d["Created Date"]);
+        return date.getHours();
     });
     var hourGroup = hour.group();
   
   
     var borough = ndx.dimension(function (d) {
-        return d.borough;
+        return d["Park Borough"];
     });
     var boroughGroup = borough.group();
 	
 
     // counts per weekday
     var dayOfWeek = ndx.dimension(function (d) {
-        var day = d.dd.getDay();
-        var name=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-        return day+"."+name[day];
+        var date = new Date(d["Created Date"]);
+        var day = date.getDay();
+        var dayNames=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+        return day+"."+dayNames[day];
     });
     var dayOfWeekGroup = dayOfWeek.group();
 
 
 	var zipcode = ndx.dimension(function(d){
-		return d.zipcode
+        if(d["Incident Zip"]==""){
+            return "NA"
+        }
+		return d["Incident Zip"]
 	})
 	var zipcodeGroup = zipcode.group();
 
 	var topRowHeight = 120;
 	var topRowColor = "#EDA929"
+    
+    //var duration = ndx.dimension(function(d){
+    //    var start = new Date(d["Created Date"])
+    //    
+    //    if(d["Closed Date"]==""){
+    //        var end = new Date()
+    //    }else{
+    //        var end = new Date( d["Closed Date"])
+    //    }
+    //    
+    //    var duration = (end.getTime()-start.getTime())/1000/60/60
+    //    console.log(duration)
+    //    return duration
+    //})
+   //var durationGroup = duration.group()
+   //
+   //durationChart.width(300)
+   //    .height(topRowHeight)
+   //    .margins({top: 0, right: 50, bottom: 20, left: 50})
+   //    .ordinalColors([topRowColor])
+   //    .dimension(duration)
+   //    .group(durationGroup)
+   //    .centerBar(true)
+   //  //  .elasticY(true)
+   //	
+   //    .gap(1)
+   //    .x(d3.scale.linear().domain([0,20]))
+   //    .yAxis().ticks(4);
+   //
+    
     dayOfWeekChart.width(200)
         .height(topRowHeight)
         .margins({top: 0, left: 30, right: 10, bottom: 20})
@@ -116,9 +150,7 @@ function ready(error, data, geodata){
             return d.key.split(".")[1];
         })
 		.labelOffsetX(-30)
-		//.labelOffsetY(0)
-		
-        // title sets the row text
+		.labelOffsetY(10)
         .title(function (d) {
             return d.value;
         })
@@ -126,39 +158,25 @@ function ready(error, data, geodata){
         .gap(2)
         .xAxis().ticks(4);
 		
-	hourChart.width(300)
+	hourChart.width(400)
 	        .height(topRowHeight)
 	        .margins({top: 0, right: 50, bottom: 20, left: 50})
 	        .ordinalColors([topRowColor])
 	        .dimension(hour)
 	        .group(hourGroup)
-	        .centerBar(true)
-	        .elasticY(true)
-			
+	        .centerBar(true)			
 	        .gap(1)
 	        .x(d3.scale.linear().domain([1,24]))
 	        .yAxis().ticks(4);
 			
-    tempChart.width(300)
-        .height(topRowHeight)
-        .margins({top: 20, left: 50, right: 10, bottom: 20})
-        .group(tempGroup)
-        .dimension(temp)
-        .ordinalColors([topRowColor])
-        .centerBar(true)
-        .gap(1)
-        .x(d3.scale.linear().domain([1,124]))
-        .yAxis().ticks(4)
-		
+   
 		var boroughScale = d3.scale.linear().domain([0,7]).range(["#ffffff", "#dc3a23"])
 		var boroughChartColors = []
 		for(var i =2; i < 7; i ++){
 			boroughChartColors.push(boroughScale(i))
 		}
 		
-		//TODO:lenght of current data
 		var allValue = data.length
-		//console.log(allvalue)
 		
     boroughChart.width(220)
         .height(140)
@@ -177,7 +195,6 @@ function ready(error, data, geodata){
 		.labelOffsetX(-90)
 		.labelOffsetY(15)
         .label(function (d) {
-			//return toTitleCase(d.key) + ' ' + Math.round((d.endAngle - d.startAngle) / Math.PI * 50) + '%';
             return toTitleCase(d.key+ "  "+Math.round(d.value/allValue*100)+"%");
         })
         .xAxis().ticks(2)
@@ -196,8 +213,6 @@ function ready(error, data, geodata){
 	    })
 		.labelOffsetX(-35)
 		.labelOffsetY(12)
-		
-	    // title sets the row text
 	    .title(function (d) {
 	        return d.value;
 	    })
@@ -251,75 +266,12 @@ function ready(error, data, geodata){
     dc.dataCount(".dc-data-count")
         .dimension(ndx)
         .group(all)
-        // (optional) html, for setting different html for some records and all records.
-        // .html replaces everything in the anchor with the html given using the following function.
-        // %filter-count and %total-count are replaced with the values obtained.
         .html({
             some:"%filter-count selected out of <strong>%total-count</strong> records | <a href='javascript:dc.filterAll(); dc.renderAll();''>Reset All</a>",
             all:"All  %total-count records selected."
         })
-    /*
-    //#### Data Table
-    // Create a data table widget and use the given css selector as anchor. You can also specify
-    // an optional chart group for this chart to be scoped within. When a chart belongs
-    // to a specific group then any interaction with such chart will only trigger redraw
-    // on other charts within the same chart group.
-    <!-- anchor div for data table -->
-    <div id="data-table">
-        <!-- create a custom header -->
-        <div class="header">
-            <span>Date</span>
-            <span>Open</span>
-            <span>Close</span>
-            <span>Change</span>
-            <span>Volume</span>
-        </div>
-        <!-- data rows will filled in here -->
-    </div>
-    */
- 
- /*   dc.dataTable(".dc-data-table")
-        .dimension(dateDimension)
-        // data table does not use crossfilter group but rather a closure
-        // as a grouping function
-        .group(function (d) {
-            var format = d3.format("02d");
-            return d.dd.getFullYear() + "/" + format((d.dd.getMonth() + 1));
-        })
-        .size(250) // (optional) max number of records to be shown, :default = 25
-        // dynamic columns creation using an array of closures
-        .columns([
-            function (d) {
-                return d.date;
-            },
-            function (d) {
-                return d.agency;
-            },
-            function (d) {
-                return d.zipcode;
-            },
-            function (d) {
-                return toTitleCase(d.borough);
-            },
-            function (d) {
-                return d.description;
-            }
-        ])
-        // (optional) sort using the given field, :default = function(d){return d;}
-        .sortBy(function (d) {
-            return d.dd;
-        })
-        // (optional) sort order, :default ascending
-        .order(d3.ascending)
-        // (optional) custom renderlet to post-process chart using D3
-        .renderlet(function (table) {
-            table.selectAll(".dc-table-group").classed("info", true);
-        });
 
-*/
-	//nycMap
-	//var colorScale = d3.scale.linear().domain([0,10000]).range(["#eee", "#dc3a23"])
-	var maxZipcode = zipcodeGroup.top(1)[0].value
+ 	var maxZipcode = zipcodeGroup.top(1)[0].value
 	var projection = d3.geo.mercator()
 					.center([-74.25,40.915])
 					.translate([0, 0])
@@ -333,16 +285,15 @@ function ready(error, data, geodata){
         .group(zipcodeGroup) // set crossfilter group
         //.colors(function(d, i){return  colorScale(d.value);})
 		//.colorAccessor(function(d){return d.value})
-		.colors(d3.scale.sqrt().domain([0,maxZipcode*.8]).range(["#ffffff", "#dc3a23"]))
+		.colors(d3.scale.sqrt().domain([0,maxZipcode*.8]).range(["#dddddd", "red"]))
 		.overlayGeoJson(geodata.features, "zipcode", function(d) {
             return d.properties.postalCode;
         })
 		//.on('mouseover', tip.show)
 		.legend(dc.legend().x(400).y(10).itemHeight(13).gap(5))
+		
     dc.renderAll();
 	d3.select("#loader").remove();
 };
 
-//#### Version
-//Determine the current version of dc with `dc.version`
 d3.selectAll("#version").text(dc.version);
